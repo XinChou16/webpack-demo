16555808d907ac3d6c1c0f233abcb30defddeb18
xinchou16 <zhouxinmine@live.com>
2018-04-16 21:08:58 +0800

    add demos

--- README.md
+++ README.md
@@ -6,6 +6,8 @@
 
 此时的配置文件
 
+入口文件指的是webpack读取并进行打包的原始文件，后面的打包文件需要基于这个原始文件
+
 ```js
 module.exports = {
   entry: './main.js', // 入口文件
@@ -17,6 +19,7 @@ module.exports = {
 
 ### 2. Multi-entry files
 
+多个入口文件适用于多页面的应用。这类应用每个都有不同的入口文件
 
 此时的配置文件
 
@@ -117,3 +120,24 @@ module.exports = {
 ### Reference
 
 [github](https://github.com/ruanyf/webpack-demos#demo01-entry-file-source)
+
+## Webpack
+
+webpack是一个适用于现代JS应用的静态模块打包器，包括四个核心概念
+- Entry
+- Output
+- Loaders
+- plugins 
+
+## Entry
+
+`Entry`表示webpack的入口文件，根据该属性提供的文件来进行打包，可以设置路径信息，例如一个简单的打包入口例子
+
+```js
+module.exports = {
+  entry: "./path/path1/main.js"
+}
+```
+
+
+
---
+++ demo-06/webpack.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  entry: {
+    'bundle': './main.jsx'
+  },
+  output:{
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js[x]?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015', 'react']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
